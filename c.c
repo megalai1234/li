222b0afeeaccca2da22e1597247494b59a7e9d5b
megalai1234 <43366156+megalai1234@users.noreply.github.com>
2019-03-05 12:06:49 +0530

    Create c.c

---
+++ c.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#define TOTAL_ELEMENTS (sizeof(array) / sizeof(array[0]))
+int array[] = {23,34,12,17,204,99,16};
+void main()
+{
+    int d;
+
+    for(d=-1;d <= (TOTAL_ELEMENTS-2);d++)
+        printf("%d\n",array[d+1]);
+
+    getch() ;
+}
